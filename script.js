b163478a83673484f000dfedaf617a895364eb51
IndexCorp <vilpages@gmail.com>
2024-03-03 01:06:15 +0900

    complete change

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ Telegram.WebApp.onEvent('themeChanged', function() {
     document.body.setAttribute('style', '--bg-color:' + Telegram.WebApp.backgroundColor);
 });
 
+// Function to change active tab and display corresponding content
+function changeTab(tabIndex) {
+  // Get all tabs and content elements
+  var tabs = document.querySelectorAll('.tab');
+  var tabContents = document.querySelectorAll('.tab-content');
+
+  // Remove 'active' class from all tabs and content
+  tabs.forEach(function(tab) {
+      tab.classList.remove('active');
+  });
+  tabContents.forEach(function(content) {
+      content.classList.remove('active');
+  });
+
+  // Add 'active' class to the clicked tab and corresponding content
+  tabs[tabIndex].classList.add('active');
+  tabContents[tabIndex].classList.add('active');
+}
