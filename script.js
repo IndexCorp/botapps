e3dfd7f1271de7f636afb520c88b0d8c74cbaa84
IndexCorp <vilpages@gmail.com>
2024-03-03 00:34:15 +0900

    complete change

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ Telegram.WebApp.onEvent('themeChanged', function() {
     document.documentElement.className = Telegram.WebApp.colorScheme;
 });
 
-
 Telegram.WebApp.setHeaderColor('secondary_bg_color');
 
 setViewportData();
@@ -14,4 +13,23 @@ Telegram.WebApp.onEvent('viewportChanged', setViewportData);
 
 Telegram.WebApp.onEvent('themeChanged', function() {
     document.body.setAttribute('style', '--bg-color:' + Telegram.WebApp.backgroundColor);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var tabItems = document.querySelectorAll('.tab-item');
+ 
+  tabItems.forEach(function(tab) {
+    tab.addEventListener('click', function() {
+      var tabId = this.getAttribute('data-tab');
+      var content = document.getElementById(tabId);
+ 
+      // Удаляем активный класс у текущих вкладок и контента
+      document.querySelector('.tab-item.active').classList.remove('active');
+      document.querySelector('.tab-pane.active').classList.remove('active');
+ 
+      // Добавляем активный класс для выбранной вкладки и контента
+      this.classList.add('active');
+      content.classList.add('active');
+    });
+  });
 });
